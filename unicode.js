34d55d348b773cb91777b31485a23fbb11879ebe
Jin <142944786+ElsieElsevier@users.noreply.github.com>
2026-08-08 04:59:01 -0500

    Fixed JavaScript

--- unicode.js
+++ unicode.js
@@ -90,7 +90,7 @@ let firstCodePoint = inputString1.charCodeAt(0);
 let thirdCodePoint = inputString1.charCodeAt(2);
 
 // Task 2
-let wordFromCodePoints = String.fromCharCode(72,101,108,108);
+let wordFromCodePoints = String.fromCharCode(74,97,118,97,83,99,114,105,112,116);
 
 // Task 3
 let inputString2 = "Launch";
